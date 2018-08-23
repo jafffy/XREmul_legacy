ab672cb0675280bbb378e2e69dc97c547409f22f
Jaewon Choi <jaewon.james.choi@gmail.com>
2018-08-23 19:24:29 +0900

    Implements FPS Camera based on XBox One Controller

---
+++ Assets/Scripts/XREmul/TracerCoreEditor.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TracerCore))]
+public class TracerCoreEditor : Editor {
+    enum PlayerState { Idle, Playing, Paused };
+    enum RecorderState { Idle, Recording, Paused }
+
+    PlayerState playerState = PlayerState.Idle;
+    RecorderState recorderState = RecorderState.Idle;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        var tracerCore = (TracerCore)target;
+
+        GUILayout.Label(string.Format("Player State: {0}", playerState.ToString()));
+
+        if (playerState == PlayerState.Idle)
+        {
+            if (GUILayout.Button("Play"))
+            {
+                playerState = PlayerState.Playing;
+            }
+        }
+        else if (playerState == PlayerState.Playing)
+        {
+            if (GUILayout.Button("Pause"))
+            {
+                playerState = PlayerState.Paused;
+            }
+
+            if (GUILayout.Button("Stop"))
+            {
+                playerState = PlayerState.Idle;
+            }
+        }
+        else if (playerState == PlayerState.Paused)
+        {
+            if (GUILayout.Button("Resume"))
+            {
+                playerState = PlayerState.Playing;
+            }
+
+            if (GUILayout.Button("Stop"))
+            {
+                playerState = PlayerState.Idle;
+            }
+        }
+
+        GUILayout.Label(string.Format("Recorder State: {0}", recorderState.ToString()));
+
+        if (recorderState == RecorderState.Idle)
+        {
+            if (GUILayout.Button("Record"))
+            {
+                recorderState = RecorderState.Recording;
+            }
+        }
+        else if (recorderState == RecorderState.Recording)
+        {
+            if (GUILayout.Button("Pause"))
+            {
+                recorderState = RecorderState.Paused;
+            }
+
+            if (GUILayout.Button("Stop"))
+            {
+                recorderState = RecorderState.Idle;
+            }
+        }
+        else if (recorderState == RecorderState.Paused)
+        {
+            if (GUILayout.Button("Resume"))
+            {
+                recorderState = RecorderState.Recording;
+            }
+
+            if (GUILayout.Button("Stop"))
+            {
+                recorderState = RecorderState.Idle;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/XREmul/XBoxOneController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XInputDotNetPure;
+
+public class XBoxOneController : MonoBehaviour {
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state, prevState;
+
+    float rotationX = 0.0f, rotationY = 0.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+
+        var rotateVector = new Vector2(state.ThumbSticks.Left.X, state.ThumbSticks.Left.Y);
+
+        float sensitivityX = 5.0f, sensitivityY = 5.0f;
+
+        rotationX = transform.localEulerAngles.y + rotateVector.x * sensitivityX;
+        rotationY += rotateVector.y * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, -80.0f, 80.0f);
+        transform.localEulerAngles = new Vector3(-rotationY, rotationX);
+
+        transform.localPosition +=
+            state.ThumbSticks.Right.Y * transform.forward * 50.0f * Time.deltaTime
+            + state.ThumbSticks.Right.X * transform.right * 50.0f * Time.deltaTime;
+    }
+}
