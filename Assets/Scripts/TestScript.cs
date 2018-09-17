7410366a5d4d4c564d58c349dad7e1347c998b47
DESKTOP-N3UA55V\Chan <cliwo@naver.com>
2018-09-18 01:18:32 +0900

    1. Leap Frame / Vector Hand / Hand 에 대해서 더 알아보기. Frame에 존재하는 hand 데이터를 replay할 수 없을까?

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEditor;
+
+using Leap.Unity;
+using Leap;
+using Leap.Unity.Encoding;
+
+public class TestScript : MonoBehaviour {
+
+    public LeapXRServiceProvider provider;
+
+    public CapsuleHand rightHand;
+
+    Frame capturedFrame;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CaptureFrame()
+    {
+        capturedFrame = provider.CurrentFrame;
+        Debug.Log("frame " + capturedFrame.Id);
+    }
+
+    public void TransformFrame()
+    {
+        Hand source = HandUtils.Get(capturedFrame, Chirality.Right);
+
+        rightHand.SetLeapHand(source);
+        //VectorHand vHand = new VectorHand(source);
+        //vHand.Decode(HandUtils.Get(provider.CurrentFrame, Chirality.Right));
+
+        EditorApplication.isPaused = true;
+    }
+}
