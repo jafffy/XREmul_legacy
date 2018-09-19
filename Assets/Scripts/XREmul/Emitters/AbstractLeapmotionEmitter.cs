db5a2248f9b278af70d2296c65a22ad9a84735f8
Seungchan Jeong cliwo@naver.com <cliwo@naver.com>
2018-09-19 21:05:48 +0900

    Refractoring on progress

---
+++ Assets/Scripts/XREmul/Emitters/AbstractLeapmotionEmitter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Leap;
+using Leap.Unity;
+
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Text;
+public abstract class AbstractLeapmotionEmitter : AbstractRecorder {
+
+	public struct FrameData
+    {
+		public Frame frame;
+    }
+
+    abstract public FrameData frameData { get; }
+
+    internal override string GetRecordEntry()
+    {
+        FrameData data = frameData;
+		return SerializableToCSV.Serialize(data.frame);
+    }
+}
+
--- Assets/Scripts/XREmul/Interfaces/LeapSkeleton.cs
+++ Assets/Scripts/XREmul/Interfaces/LeapSkeleton.cs
@@ -12,12 +12,7 @@ public class LeapSkeleton : MonoBehaviour {
     }
     void Update()
     {
-        //실제 스켈레톤을 Play시 움직이는 코드가 필요
-        //하지만 원래 Interface의 목적은 Camera가 아닌 물체가 Camera를 움직이는 것 처럼
-        //input 이 다른 device를 통해 전달될 떄 필요하다
-        //말이 어려운데 아무튼 이 말의 의미는, 이 LeapSkeleton이 꼭 필요할까..? 이다..
-        //마우스 같은게 Leap Skeleton을 움직일 수 있으면 모르겠는데
-        //실질적으로 어려울 것 같다
-
+        //? : 이 코드가 필요할까? 어짜피 Leap Model들은 Leap에 의해 제어
+        //+ 다른 Input을 통해 Leap을 제어하기는 어려워 보인다 
     }
 }
