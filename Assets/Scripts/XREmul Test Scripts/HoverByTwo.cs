02b22117b345ebc60a6bf7c743c66ae3e3a1e46f
DESKTOP-N3UA55V\Chan <cliwo@naver.com>
2018-09-27 00:32:09 +0900

    Hover Test Scenes

---
+++ Assets/Scripts/XREmul Test Scripts/HoverByTwo.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Leap.Unity.Interaction;
+public class HoverByTwo : MonoBehaviour {
+
+    private InteractionBehaviour _intObj;
+    private Material _material;
+
+    // Use this for initialization
+    void Start () {
+        _intObj = GetComponent<InteractionBehaviour>();
+
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer == null)
+        {
+            renderer = GetComponentInChildren<Renderer>();
+        }
+        if (renderer != null)
+        {
+            _material = renderer.material;
+        }
+
+        _intObj.OnHoverStay += Shining;
+    }
+	
+    void Shining()
+    {
+        if(_intObj.hoveringControllers.Count == 2)
+        {
+            _material.color = new Color(Random.Range(0f, 1f),
+                Random.Range(0f, 1f), Random.Range(0f, 1f));
+        }
+    }
+
+}
